fca1921325d3eddf85899f89c4f623f6fb10ec15
Carl Jones <c.m.jones@dur.ac.uk>
2019-09-28 14:01:50 +0200

    [doc] Expand on the purpose of experiment_structure

--- src/experiment_structure.rs
+++ src/experiment_structure.rs
@@ -1,25 +1,38 @@
 /// Grouping and organisation of the experiment files
 ///
-/// # ExperimentPart
+/// # Outline
 ///
-/// This contains a single step on pipeline, for instance a input directory,
-/// script to run and the expected output files, but also a means of printing
-/// the information about the creation date of the information
+/// We expect a directory that holds all of the steps in the pipeline for a single
+/// experiment; for instance we may have: 1) data extracted from images; 2)
+/// filtering and aggregation of this data 3) plots created from the previous
+/// step. The output for each step of the analysis should be organised into
+/// sub-directories.
 ///
-/// # Experiment
+/// Each of these steps would correspond to a ExperimentPart, grouped together
+/// into a single Experiment.
 ///
-/// Root structure for the run, containing the configuration file and a vector
-/// of ``ExperimentPart``s.
+/// Typically a new experiment directory may use different starting data or a
+/// different analysis step, for which a separate Experiment struct is created.
 ///
+/// ## Experiment
+///
+/// Container for all of the steps in the pipeline for one experiment.
+/// Many of these may be created to allow
+///
+/// # Future work
 /// Later we expect to load these values from a configuration files but for now
 /// the values are hard coded.
 
-use std::path::{PathBuf,Path};
+use std::path::{PathBuf};
 use crate::file_status as fs;
 
-/// Container for the single step of the pipeline
+/// This contains a single step on pipeline. This will likely contain a list of
+/// expected output files. However, if these don't exist then we should provide
+/// a means of creating these files.
 ///
-/// # arg
+/// For now this prints summary information about the output files, whether they
+/// are up to date and how this compares against the source code.
+///
 pub struct ExperimentPart {
     name: String,
     pub file_list: Vec<fs::ExperimentFile>,
@@ -37,24 +50,31 @@ impl ExperimentPart {
         ExperimentPart { name, file_list}
     }
 
+    /// Print information about the number and age of files in the Part
     pub fn create_summary(&self) -> String {
         // Spaces used to indent secondary lines
         let indent = "    ";
 
-        let mut summary = format!("{} contains {} files",
+        let summary = format!(
+            "{} contains {} file",
             &self.name,
             &self.file_list.len(),
         );
 
-        let newest_summary = format!("\n{}Newest file modified at {}",
-                                  indent,
-                                  &self.get_newest_file().formatted_time()
+        let newest_summary = format!(
+            "\n{}Newest file modified at {}",
+            indent,
+            &self.get_newest_file().formatted_time()
         );
 
-        let oldest_summary = format!("\n{}Oldest file modified at {}",
-                                  indent,
-                                  &self.get_oldest_file().formatted_time()
-        );
+        let mut oldest_summary = String::from("");
+        if &self.file_list.len() > &1 {
+            oldest_summary += &format!(
+                "\n{}Oldest file modified at {}",
+                indent,
+                &self.get_oldest_file().formatted_time()
+            );
+        };
 
         summary + &newest_summary + &oldest_summary
     }
@@ -68,4 +88,3 @@ impl ExperimentPart {
     }
 
 }
-
